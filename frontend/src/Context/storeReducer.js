adabeae41025123ed6921a017093a60506dcede0
Sai Bannur <bannursai@gmail.com>
2023-06-05 17:11:41 -0700

    Added add/remove to cart in product screen

--- frontend/src/Context/storeReducer.js
+++ frontend/src/Context/storeReducer.js
@@ -4,7 +4,6 @@ export const storeReducer = (state, action) => {
     switch(action.type){
         case 'ADD_TO_CART':
             const newItem = action.payload;
-            console.log("DATA", state.cart.cartItems);
             const existItem = state.cart.cartItems.find((item) => item._id === newItem._id);
 
             const cartItems = existItem ? state.cart.cartItems.map((item) => item._id === existItem._id ? newItem : item) : [...state.cart.cartItems, newItem]
@@ -20,7 +19,6 @@ export const storeReducer = (state, action) => {
             return { ...state, cart: { ...state.cart, cartItems } };
           }
 
-        
         case 'SIGN_IN':
             console.log("SIGN_IN Reducer");
             return;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useContext } from 'react';
 import axios from 'axios';
 import {Container, Row, Col } from 'react-bootstrap';
+import { Store } from '../Context/storeContext'
 
 
 const reducer = (state, action) => {
@@ -32,7 +33,28 @@ export default function ProductScreen() {
 
     const param = useParams().slug;
     const [state, dispatch] = useReducer(reducer, {loader: false, product: [], error: ""});
+    const {state: stxte, dispatch: dxpatch } = useContext(Store);
 
+
+  const addToCart = () => {  
+    dxpatch({type: "ADD_TO_CART", payload: state.product}) 
+   } 
+ 
+   const removeFromCart = () => {
+    dxpatch({type: "REMOVE_FROM_CART", payload: state.product})  
+   }
+ 
+   const inCart = (propItem) => {
+    console.log("State", state)
+    console.log("Stxte", stxte)
+
+     const data = stxte.cart.cartItems;
+     if( data.find(item => item._id === propItem._id)){
+         return true;
+     }
+     return false;
+   }
+ 
     useEffect(() => {
 
         const fetchProduct = async () => {
@@ -62,7 +84,8 @@ export default function ProductScreen() {
               <h5>Price: ${state.product.price}</h5><hr></hr>
               <h5>Review: <strong>{state.product.rating} / 5</strong> ({state.product.numReviews} Reviews)</h5> <hr></hr>
               <h5>Description: {state.product.description}</h5> <hr></hr>
-              <h5>Status: {state.product.inStock > 0 ? <span variant="success" class="badge bg-success">Available</span>:<span variant="danger" class="badge bg-danger">Out of Stock</span>}</h5> <hr></hr>
+              <h5>Status: {state.product.inStock > 0 ? <span variant="success" className="badge bg-success">Available</span>:<span variant="danger" className="badge bg-danger">Out of Stock</span>}</h5> <hr></hr>
+              {inCart(state.product) ? <button className="removeCart" onClick={removeFromCart}>Remove from Cart</button> : <button className="addCart" onClick={addToCart}>Add to Cart</button>}
               </Col>
             </Row>
           </Container>
